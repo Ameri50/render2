052b31c011c4315e00af1355e1a3328aff477847
Ameri50 <ssj52949gmall.com>
2025-04-02 17:08:18 -0500

    correguido

--- conexion.php
+++ conexion.php
@@ -1,15 +1,18 @@
 <?php
 
-function conexion(){
+function conexion() {
+    $host = "host=ddpg-cvmpjc7diees73bpro8g-a.oregon-postgres.render.com";
+    $port = "port=5432";
+    $dbname = "dbname=test_m6ty";
+    $user = "user=rojas";
+    $password = "password=VT9jKLUtEu0QpUKrVzJz1m6sobuU3ov7";
 
-$host = "host=ddpg-cvmpjc7diees73bpro8g-a.oregon-postgres.render.com";
-$port = "port=5432";
-$dbname = "dbname=test_m6ty";
-$user = "user=rojas";
-$password = "password=VT9jKLUtEu0QpUKrVzJz1m6sobuU3ov7";
+    $db = pg_connect("$host $port $dbname $user $password");
 
-$db = pg_connect("$host $port $dbname $user $password");
+    if (!$db) {
+        die("Error: No se pudo conectar a la base de datos.");
+    }
 
-return $db;
+    return $db;
 }
 ?>
